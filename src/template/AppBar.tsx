82d68d5251a4a28832be98123681aad2f2ea11e6
Dianliang233 <dianliang233@gmail.com>
2022-05-20 11:41:14 +0800

    Add CompositeMenu

--- src/template/AppBar.tsx
+++ src/template/AppBar.tsx
@@ -10,18 +10,9 @@ import {
 } from '@mui/material'
 import { useState, MouseEvent } from 'react'
 import { Github } from 'mdi-material-ui'
+import CompositeMenu from './CompositeMenu'
 
 export default function MemeAppBar() {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-
-  const handleMenu = (event: MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
   const theme = createTheme({
     palette: {
       primary: {
@@ -37,26 +28,10 @@ export default function MemeAppBar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             梗体中文 · 在线构建
           </Typography>
-          <Button onClick={handleMenu} color="inherit" startIcon={<Github />}>
-            GitHub
-          </Button>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-          </Menu>
+
+          <CompositeMenu icon={<Github />} items={[
+            { name: 'Java 版', href: 'https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack' }
+          ]}>GitHub</CompositeMenu>
         </Toolbar>
       </AppBar>
     </ThemeProvider>
