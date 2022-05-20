e18ed766a560677814746670b6fbf13565a713c6
Dianliang233 <dianliang233@gmail.com>
2022-05-20 20:12:58 +0800

    Add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
 import './App.css'
-import { useState } from 'react'
 import MemeAppBar from './template/AppBar'
-import * as React from 'react'
+import { useMemo } from 'react'
 import {
   CssBaseline,
   ThemeProvider,
   createTheme,
   useMediaQuery,
 } from '@mui/material'
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import en from './locales/en.json'
+import zhHans from './locales/zh-hans.json'
+import zhMeme from './locales/zh-meme.json'
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      main: en,
+    },
+    zhHans: {
+      main: zhHans,
+    },
+    zhMeme: {
+      main: zhMeme,
+    },
+  },
+  ns: ['main'],
+  defaultNS: 'main',
+  lng: 'zhHans',
+  fallbackLng: ['zhMeme', 'en', 'zhHans'],
+
+  interpolation: {
+    escapeValue: false,
+  },
+})
 
 function App() {
-  const [count, setCount] = useState(0)
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
 
-  const theme = React.useMemo(
+  const theme = useMemo(
     () =>
       createTheme({
         palette: {
--- src/template/AppBar.tsx
+++ src/template/AppBar.tsx
@@ -1,14 +1,19 @@
+import { ReactNode, useState, MouseEvent } from 'react'
 import {
   ThemeProvider,
   AppBar,
+  Button,
   Toolbar,
   Typography,
+  Menu,
+  MenuItem,
   createTheme,
 } from '@mui/material'
-import { Github } from 'mdi-material-ui'
-import CompositeMenu from './CompositeMenu'
+import { Github, Disc, Post } from 'mdi-material-ui'
+import { useTranslation } from 'react-i18next'
 
 export default function MemeAppBar() {
+  const { t } = useTranslation()
   const theme = createTheme({
     palette: {
       primary: {
@@ -22,15 +27,124 @@ export default function MemeAppBar() {
       <AppBar position="sticky" color="primary">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            梗体中文 · 在线构建
+            {t('appbar.title')}
           </Typography>
-
-          <CompositeMenu icon={<Github />} items={[
-            { name: 'Java 版', href: 'https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack' },
-            { name: '基岩版', href: 'https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack-bedrock' }
-          ]}>GitHub</CompositeMenu>
+          <BarLinks />
         </Toolbar>
       </AppBar>
     </ThemeProvider>
+  )
+}
+
+function BarLinks() {
+  const { t } = useTranslation()
+
+  return (
+    <>
+      <CompositeMenu
+        icon={<Post />}
+        items={[
+          {
+            name: t('java'),
+            href: 'https://www.mcbbs.net/thread-1004643-1-1.html',
+          },
+          {
+            name: t('bedrock'),
+            href: 'https://www.mcbbs.net/thread-1005191-1-1.html',
+          },
+        ]}
+      >
+        {t('appbar.mcbbs')}
+      </CompositeMenu>
+      <CompositeMenu
+        icon={<Github />}
+        items={[
+          {
+            name: t('java'),
+            href: 'https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack',
+          },
+          {
+            name: t('bedrock'),
+            href: 'https://github.com/Teahouse-Studios/mcwzh-meme-resourcepack-bedrock',
+          },
+        ]}
+      >
+        {t('appbar.github')}
+      </CompositeMenu>
+      <CompositeMenu
+        icon={<Disc />}
+        items={[
+          {
+            name: t('java'),
+            href: 'https://wdf.ink/record-java',
+          },
+          {
+            name: t('bedrock'),
+            href: 'https://wdf.ink/record-bedrock',
+          },
+        ]}
+      >
+        {t('appbar.discPack')}
+      </CompositeMenu>
+    </>
+  )
+}
+
+interface CompositeMenuProps {
+  children: ReactNode
+  icon: JSX.Element
+  items: {
+    name: string
+    href: string
+  }[]
+}
+
+function CompositeMenu(props: CompositeMenuProps) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+
+  const handleMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const openHref = (href: string) => {
+    window.open(href, '_blank')
+    handleClose()
+  }
+
+  return (
+    <>
+      <Button
+        onClick={handleMenu}
+        color="inherit"
+        startIcon={props.icon}
+        sx={{ ml: 1 }}
+      >
+        {props.children}
+      </Button>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {props.items.map((item) => (
+          <MenuItem onClick={() => openHref(item.href)} key={item.name}>
+            {item.name}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
   )
 }
