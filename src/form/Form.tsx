7685045d1614948f6c84c9058048b545b3a25792
Dianliang233 <dianliang233@gmail.com>
2022-05-22 16:59:49 +0800

    Remove unneeded ids

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -170,13 +170,7 @@ function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props
 
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
+    <div role="tabpanel" hidden={value !== index} {...other}>
       {value === index && (
         <Box sx={{ p: 3 }}>
           <Typography>{children}</Typography>
