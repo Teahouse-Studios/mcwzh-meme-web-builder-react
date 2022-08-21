d002491b7875fa95c29eaf040f3e8a3ab5bdf03a
Dianliang233 <dianliang233@gmail.com>
2022-08-21 16:14:25 +0800

    Add closable Accordion

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -1,6 +1,5 @@
 import {
   Alert,
-  AlertTitle,
   Box,
   Tabs,
   Tab,
@@ -9,31 +8,17 @@ import {
   Container,
   CircularProgress,
   Divider,
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Paper,
-  useTheme,
   IconButton,
   Tooltip,
-  Link,
 } from '@mui/material'
 import {
   Coffee,
   Devices,
   Alert as AlertIcon,
   CloseCircle,
-  ChevronDown,
-  Check,
-  Download,
   Bug,
-  ShareVariant,
-  Heart,
-  HeartBroken,
   Refresh,
-  Disc,
-  HelpCircle,
-  ContentCopy,
+  CloseCircleMultipleOutline,
 } from 'mdi-material-ui'
 import {
   useState,
@@ -41,21 +26,18 @@ import {
   ReactNode,
   MouseEventHandler,
   SyntheticEvent,
-  Dispatch,
-  SetStateAction,
   useRef,
   lazy,
   Suspense,
 } from 'react'
-import { css } from '@emotion/react'
 import { useSnackbar } from 'notistack'
-import { Trans, useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import JavaForm from './JavaForm'
 import BedrockForm from './BedrockForm'
+import LogAccordion from './LogAccordion'
 const SponsorsList = lazy(() => import('./SponsorsList'))
 import type { MemeApi, BuildLog } from './types'
 import fakeApiData from './fakeApiData'
-import allowTracking from '../tracking'
 import endpoint from '../api'
 import { useEffectOnce, useLocalStorage } from 'usehooks-ts'
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -254,9 +236,28 @@ export default function Form() {
         {logs.length > 0 && (
           <Container id="build-logs" ref={logRootRef}>
             <Divider sx={{ mb: 2 }} />
-            <Typography variant="h5" component="h2" sx={{ mb: 2 }}>
-              {t('log.headline')}
-            </Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                mb: 2,
+              }}
+            >
+              <Typography variant="h5" component="h2">
+                {t('log.headline')}
+              </Typography>
+              <Tooltip title={t('form.clearAll')}>
+                <IconButton
+                  color="error"
+                  onClick={() => {
+                    setLogs([])
+                  }}
+                >
+                  <CloseCircleMultipleOutline />
+                </IconButton>
+              </Tooltip>
+            </Box>
             <Box>
               {logs
                 .slice()
@@ -265,6 +266,9 @@ export default function Form() {
                   <LogAccordion
                     key={log.time}
                     log={log}
+                    deleteSelf={() => {
+                      setLogs(logs.filter((l, i) => i !== index))
+                    }}
                     adLS={adLS}
                     setLS={setLS}
                     adSettings={adSettings}
@@ -293,336 +297,6 @@ export default function Form() {
         </Suspense>
       </Container>
     </>
-  )
-}
-
-function LogAccordion({
-  log,
-  adLS,
-  setLS,
-  adSettings,
-  setAdSettings,
-  defaultExpanded,
-}: {
-  log: BuildLog
-  adLS: { shown: boolean; lastShown: number; clicked: boolean }
-  setLS: Dispatch<SetStateAction<typeof adLS>>
-  adSettings: { shouldDisplayAd: boolean; adType: AdType }
-  setAdSettings: Dispatch<SetStateAction<typeof adSettings>>
-  defaultExpanded: boolean
-}) {
-  const { t } = useTranslation()
-  const theme = useTheme()
-  const { enqueueSnackbar } = useSnackbar()
-  const [expanded, setExpanded] = useState(defaultExpanded)
-  const [shareCopiedToClipboard, setShareCopiedToClipboard] = useState(false)
-  const shareUrl = async (url: string) => {
-    if (allowTracking) window.gtag('event', 'share')
-
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-    if (navigator.share) {
-      try {
-        await navigator.share({
-          title: '梗体中文构建配置分享',
-          text: '你的好友给你分享了 ta 他的梗体中文！此链接 7 日内有效：',
-          url: url,
-        })
-      } catch (err) {
-        if ((err as Error).name !== 'AbortError') {
-          throw err
-        }
-      }
-    } else {
-      await navigator.clipboard.writeText(url)
-      setShareCopiedToClipboard(true)
-      setTimeout(() => {
-        setShareCopiedToClipboard(false)
-      }, 3000)
-    }
-  }
-
-  return (
-    <Accordion key="log.time" expanded={expanded}>
-      <AccordionSummary
-        expandIcon={<ChevronDown />}
-        onClick={() => setExpanded(!expanded)}
-      >
-        <Typography
-          sx={{
-            width: '33%',
-            flexShrink: 0,
-            color: `${log.status}.main`,
-          }}
-        >
-          {log.status === 'success' ? (
-            <Check sx={{ mr: 1, verticalAlign: 'top' }} />
-          ) : (
-            <CloseCircle sx={{ mr: 1, verticalAlign: 'top' }} />
-          )}
-          {t(`log.build${log.status.replace(/^\S/, (s) => s.toUpperCase())}`)}
-        </Typography>
-        <Typography sx={{ color: 'text.secondary' }}>
-          {new Date(log.time).toLocaleString()}
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Paper
-          elevation={0}
-          sx={{
-            pt: 2,
-            pb: 2,
-            fontFamily: 'monospace',
-            mb: 1,
-            backgroundImage:
-              'linear-gradient(rgba(0, 0, 0, 0.04), rgba(0, 0, 0, 0.04))',
-            overflowX: 'auto',
-          }}
-        >
-          <Tooltip title={t('log.logCopy')}>
-            <IconButton
-              onClick={() => {
-                void navigator.clipboard.writeText(log.log)
-              }}
-              sx={{ position: 'relative', float: 'right', right: '12px' }}
-            >
-              <ContentCopy />
-            </IconButton>
-          </Tooltip>
-          {log.log.split('\n').map((line, index) => {
-            return (
-              <Typography
-                variant="body1"
-                fontFamily="monospace"
-                key={index}
-                css={css`
-                  white-space: pre-wrap;
-                  counter-increment: line;
-                  padding-left: 3.5rem;
-                  padding-right: 2rem;
-                  transition: color, background-color 0.075s ease-in-out;
-                  &::before {
-                    content: counter(line);
-                    position: absolute;
-                    left: 2.5rem;
-                  }
-                  &:hover {
-                    background-color: ${theme.palette.action.hover};
-                  }
-                `}
-                color={
-                  {
-                    warning: 'warning.main',
-                    error: 'error.main',
-                    success: 'success.main',
-                    info: 'info.main',
-                    debug: 'text.secondary',
-                    default: 'text.primary',
-                  }[
-                    line.toLowerCase().match(/(fail|error)/)
-                      ? 'error'
-                      : line.toLowerCase().match(/(warn)/)
-                      ? 'warning'
-                      : line.toLowerCase().match(/(success|succeed)/)
-                      ? 'success'
-                      : line.toLowerCase().match(/(info)/)
-                      ? 'info'
-                      : line.toLowerCase().match(/ {4}/)
-                      ? 'debug'
-                      : 'default'
-                  ]
-                }
-              >
-                {line}
-              </Typography>
-            )
-          })}
-        </Paper>
-        {adSettings.shouldDisplayAd && (
-          <>
-            <Alert
-              severity="error"
-              icon={false}
-              sx={{
-                '& > div': {
-                  display: 'flex',
-                  flexDirection: { xs: 'column', md: 'row' },
-                },
-              }}
-            >
-              <Box sx={{ mb: 1, display: 'flex', opacity: 0.8, mr: 2 }}>
-                <Heart color="error" className="donate-button" />
-              </Box>
-              <Box>
-                <AlertTitle>{t('log.ad.title')}</AlertTitle>
-                {
-                  {
-                    [AdType.FirstTime]: t('log.ad.firstTime'),
-                    [AdType.Reconsider]: t('log.ad.reconsider'),
-                    [AdType.Renew]: t('log.ad.renew'),
-                  }[adSettings.adType]
-                }
-                <Typography
-                  variant="caption"
-                  component="p"
-                  sx={{ mt: 1, color: 'text.secondary' }}
-                >
-                  {t('log.ad.donationNotice')}
-                </Typography>
-                <Box sx={{ mt: 1 }}>
-                  <Button
-                    className="donate-button"
-                    startIcon={<Heart />}
-                    variant="contained"
-                    color="error"
-                    href="https://afdian.net/@teahouse"
-                    target="_blank"
-                    rel="noopener"
-                    onClick={() => {
-                      setAdSettings((adSettings) => {
-                        return { ...adSettings, shouldDisplayAd: false }
-                      })
-                      setLS({
-                        shown: true,
-                        lastShown: Date.now(),
-                        clicked: true,
-                      })
-                      enqueueSnackbar(t('log.ad.donateSnackbar'), {
-                        autoHideDuration: 10000,
-                        variant: 'success',
-                      })
-                    }}
-                    sx={{ mr: 1, md: 1 }}
-                  >
-                    {t('footer.donate')}
-                  </Button>
-                  <Button
-                    startIcon={<HeartBroken />}
-                    color="inherit"
-                    onClick={() => {
-                      setAdSettings((adSettings) => {
-                        return { ...adSettings, shouldDisplayAd: false }
-                      })
-                      setLS({
-                        shown: true,
-                        lastShown: Date.now(),
-                        clicked: false,
-                      })
-                      enqueueSnackbar(t('log.ad.dismissSnackbar'), {
-                        autoHideDuration: 10000,
-                        variant: 'info',
-                      })
-                    }}
-                    size="small"
-                    sx={{ mr: 1 }}
-                  >
-                    {t('log.ad.dismiss')}
-                  </Button>
-                </Box>
-              </Box>
-            </Alert>
-          </>
-        )}
-        {!adSettings.shouldDisplayAd &&
-          (log.status === 'success' ? (
-            <>
-              <Alert
-                severity="info"
-                sx={{
-                  mb: 1,
-                }}
-              >
-                <Trans i18nKey="log.followUpdates">
-                  梗体中文每时每刻都在持续迭代更新！我们推荐您在一段时间后更新您的梗体中文资源包。您也可以关注我们的
-                  <Link
-                    href="https://space.bilibili.com/406275313"
-                    target="_blank"
-                    rel="noopener"
-                  >
-                    B 站动态
-                  </Link>
-                  获得一手资讯。
-                </Trans>
-              </Alert>
-              <Button
-                variant="contained"
-                startIcon={<Download />}
-                onClick={() =>
-                  window.gtag('event', 'download', {
-                    eventType: log.platform,
-                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                    eventLabel: new URL(log.downloadUrl!).pathname,
-                  })
-                }
-                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                href={log.downloadUrl!}
-                rel="noopener"
-                target="_blank"
-                sx={{ mr: 1 }}
-              >
-                {t('log.download')}
-              </Button>
-              <Button
-                startIcon={<Disc />}
-                sx={{ mr: 1 }}
-                href={
-                  {
-                    java: 'https://wdf.ink/record-java',
-                    bedrock: 'https://wdf.ink/record-bedrock',
-                  }[log.platform]
-                }
-                target="_blank"
-                rel="noopener"
-              >
-                {t('appbar.discPack')}
-              </Button>
-              <Button
-                startIcon={
-                  shareCopiedToClipboard ? <Check /> : <ShareVariant />
-                }
-                onClick={() => {
-                  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-                  void shareUrl(log.downloadUrl!)
-                }}
-                sx={{ mr: 1 }}
-              >
-                {t(shareCopiedToClipboard ? 'log.clipboard' : 'log.share')}
-              </Button>
-              <Button
-                className="donate-button"
-                startIcon={<Heart />}
-                color="error"
-                href="https://afdian.net/@teahouse"
-                target="_blank"
-                rel="noopener"
-                sx={{ mr: 1 }}
-              >
-                {t('footer.donate')}
-              </Button>
-              <Tooltip title={t('log.howToInstall')}>
-                <IconButton
-                  href="https://lakeus.xyz/wiki/梗体中文/导入"
-                  target="_blank"
-                  rel="noopener"
-                  sx={{ float: 'right' }}
-                >
-                  <HelpCircle />
-                </IconButton>
-              </Tooltip>
-            </>
-          ) : (
-            <Button
-              variant="contained"
-              startIcon={<Bug />}
-              color="error"
-              href="https://github.com/Teahouse-Studios/mcwzh-meme-web-builder/issues/new/choose"
-              rel="noopener"
-              sx={{ mr: 1 }}
-            >
-              {t('log.feedback')}
-            </Button>
-          ))}
-      </AccordionDetails>
-    </Accordion>
   )
 }
 
---
+++ src/form/LogAccordion.tsx
@@ -0,0 +1,400 @@
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Typography,
+  Button,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Paper,
+  useTheme,
+  IconButton,
+  Tooltip,
+  Link,
+  AccordionActions,
+} from '@mui/material'
+import {
+  CloseCircle,
+  ChevronDown,
+  Check,
+  Download,
+  Bug,
+  ShareVariant,
+  Heart,
+  HeartBroken,
+  Disc,
+  HelpCircle,
+  ContentCopy,
+  Close,
+} from 'mdi-material-ui'
+import { useState, Dispatch, SetStateAction } from 'react'
+import { css } from '@emotion/react'
+import { useSnackbar } from 'notistack'
+import { Trans, useTranslation } from 'react-i18next'
+import { AdType } from './Form'
+import type { BuildLog } from './types'
+import allowTracking from '../tracking'
+
+export default function LogAccordion({
+  log,
+  adLS,
+  setLS,
+  deleteSelf,
+  adSettings,
+  setAdSettings,
+  defaultExpanded,
+}: {
+  log: BuildLog
+  adLS: { shown: boolean; lastShown: number; clicked: boolean }
+  setLS: Dispatch<SetStateAction<typeof adLS>>
+  deleteSelf: () => void
+  adSettings: { shouldDisplayAd: boolean; adType: AdType }
+  setAdSettings: Dispatch<SetStateAction<typeof adSettings>>
+  defaultExpanded: boolean
+}) {
+  const { t } = useTranslation()
+  const theme = useTheme()
+  const { enqueueSnackbar } = useSnackbar()
+  const [expanded, setExpanded] = useState(defaultExpanded)
+  const [shareCopiedToClipboard, setShareCopiedToClipboard] = useState(false)
+  const shareUrl = async (url: string) => {
+    if (allowTracking) window.gtag('event', 'share')
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: '梗体中文构建配置分享',
+          text: '你的好友给你分享了 ta 他的梗体中文！此链接 7 日内有效：',
+          url: url,
+        })
+      } catch (err) {
+        if ((err as Error).name !== 'AbortError') {
+          throw err
+        }
+      }
+    } else {
+      await navigator.clipboard.writeText(url)
+      setShareCopiedToClipboard(true)
+      setTimeout(() => {
+        setShareCopiedToClipboard(false)
+      }, 3000)
+    }
+  }
+
+  return (
+    <Accordion key="log.time" expanded={expanded}>
+      <AccordionSummary
+        expandIcon={<ChevronDown />}
+        onClick={() => setExpanded(!expanded)}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            width: '100%',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              width: '100%',
+            }}
+          >
+            <Typography
+              sx={{
+                width: '33%',
+                flexShrink: 0,
+                color: `${log.status}.main`,
+              }}
+            >
+              {log.status === 'success' ? (
+                <Check sx={{ mr: 1, verticalAlign: 'top' }} />
+              ) : (
+                <CloseCircle sx={{ mr: 1, verticalAlign: 'top' }} />
+              )}
+              {t(
+                `log.build${log.status.replace(/^\S/, (s) => s.toUpperCase())}`
+              )}
+            </Typography>
+            <Typography sx={{ color: 'text.secondary' }}>
+              {new Date(log.time).toLocaleString()}
+            </Typography>
+          </Box>
+          <AccordionActions sx={{ padding: '0' }}>
+            <IconButton
+              size="small"
+              color="error"
+              sx={{ padding: '0' }}
+              onClick={() => {
+                deleteSelf()
+              }}
+            >
+              <Close />
+            </IconButton>
+          </AccordionActions>
+        </Box>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Paper
+          elevation={0}
+          sx={{
+            pt: 2,
+            pb: 2,
+            fontFamily: 'monospace',
+            mb: 1,
+            backgroundImage:
+              'linear-gradient(rgba(0, 0, 0, 0.04), rgba(0, 0, 0, 0.04))',
+            overflowX: 'auto',
+          }}
+        >
+          <Tooltip title={t('log.logCopy')}>
+            <IconButton
+              onClick={() => {
+                void navigator.clipboard.writeText(log.log)
+              }}
+              sx={{ position: 'relative', float: 'right', right: '12px' }}
+            >
+              <ContentCopy />
+            </IconButton>
+          </Tooltip>
+          {log.log.split('\n').map((line, index) => {
+            return (
+              <Typography
+                variant="body1"
+                fontFamily="monospace"
+                key={index}
+                css={css`
+                  white-space: pre-wrap;
+                  counter-increment: line;
+                  padding-left: 3.5rem;
+                  padding-right: 2rem;
+                  transition: color, background-color 0.075s ease-in-out;
+                  &::before {
+                    content: counter(line);
+                    position: absolute;
+                    left: 2.5rem;
+                  }
+                  &:hover {
+                    background-color: ${theme.palette.action.hover};
+                  }
+                `}
+                color={
+                  {
+                    warning: 'warning.main',
+                    error: 'error.main',
+                    success: 'success.main',
+                    info: 'info.main',
+                    debug: 'text.secondary',
+                    default: 'text.primary',
+                  }[
+                    line.toLowerCase().match(/(fail|error)/)
+                      ? 'error'
+                      : line.toLowerCase().match(/(warn)/)
+                      ? 'warning'
+                      : line.toLowerCase().match(/(success|succeed)/)
+                      ? 'success'
+                      : line.toLowerCase().match(/(info)/)
+                      ? 'info'
+                      : line.toLowerCase().match(/ {4}/)
+                      ? 'debug'
+                      : 'default'
+                  ]
+                }
+              >
+                {line}
+              </Typography>
+            )
+          })}
+        </Paper>
+        {adSettings.shouldDisplayAd && (
+          <>
+            <Alert
+              severity="error"
+              icon={false}
+              sx={{
+                '& > div': {
+                  display: 'flex',
+                  flexDirection: { xs: 'column', md: 'row' },
+                },
+              }}
+            >
+              <Box sx={{ mb: 1, display: 'flex', opacity: 0.8, mr: 2 }}>
+                <Heart color="error" className="donate-button" />
+              </Box>
+              <Box>
+                <AlertTitle>{t('log.ad.title')}</AlertTitle>
+                {
+                  {
+                    [AdType.FirstTime]: t('log.ad.firstTime'),
+                    [AdType.Reconsider]: t('log.ad.reconsider'),
+                    [AdType.Renew]: t('log.ad.renew'),
+                  }[adSettings.adType]
+                }
+                <Typography
+                  variant="caption"
+                  component="p"
+                  sx={{ mt: 1, color: 'text.secondary' }}
+                >
+                  {t('log.ad.donationNotice')}
+                </Typography>
+                <Box sx={{ mt: 1 }}>
+                  <Button
+                    className="donate-button"
+                    startIcon={<Heart />}
+                    variant="contained"
+                    color="error"
+                    href="https://afdian.net/@teahouse"
+                    target="_blank"
+                    rel="noopener"
+                    onClick={() => {
+                      setAdSettings((adSettings) => {
+                        return { ...adSettings, shouldDisplayAd: false }
+                      })
+                      setLS({
+                        shown: true,
+                        lastShown: Date.now(),
+                        clicked: true,
+                      })
+                      enqueueSnackbar(t('log.ad.donateSnackbar'), {
+                        autoHideDuration: 10000,
+                        variant: 'success',
+                      })
+                    }}
+                    sx={{ mr: 1, md: 1 }}
+                  >
+                    {t('footer.donate')}
+                  </Button>
+                  <Button
+                    startIcon={<HeartBroken />}
+                    color="inherit"
+                    onClick={() => {
+                      setAdSettings((adSettings) => {
+                        return { ...adSettings, shouldDisplayAd: false }
+                      })
+                      setLS({
+                        shown: true,
+                        lastShown: Date.now(),
+                        clicked: false,
+                      })
+                      enqueueSnackbar(t('log.ad.dismissSnackbar'), {
+                        autoHideDuration: 10000,
+                        variant: 'info',
+                      })
+                    }}
+                    size="small"
+                    sx={{ mr: 1 }}
+                  >
+                    {t('log.ad.dismiss')}
+                  </Button>
+                </Box>
+              </Box>
+            </Alert>
+          </>
+        )}
+        {!adSettings.shouldDisplayAd &&
+          (log.status === 'success' ? (
+            <>
+              <Alert
+                severity="info"
+                sx={{
+                  mb: 1,
+                }}
+              >
+                <Trans i18nKey="log.followUpdates">
+                  梗体中文每时每刻都在持续迭代更新！我们推荐您在一段时间后更新您的梗体中文资源包。您也可以关注我们的
+                  <Link
+                    href="https://space.bilibili.com/406275313"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    B 站动态
+                  </Link>
+                  获得一手资讯。
+                </Trans>
+              </Alert>
+              <Button
+                variant="contained"
+                startIcon={<Download />}
+                onClick={() =>
+                  window.gtag('event', 'download', {
+                    eventType: log.platform,
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    eventLabel: new URL(log.downloadUrl!).pathname,
+                  })
+                }
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                href={log.downloadUrl!}
+                rel="noopener"
+                target="_blank"
+                sx={{ mr: 1 }}
+              >
+                {t('log.download')}
+              </Button>
+              <Button
+                startIcon={<Disc />}
+                sx={{ mr: 1 }}
+                href={
+                  {
+                    java: 'https://wdf.ink/record-java',
+                    bedrock: 'https://wdf.ink/record-bedrock',
+                  }[log.platform]
+                }
+                target="_blank"
+                rel="noopener"
+              >
+                {t('appbar.discPack')}
+              </Button>
+              <Button
+                startIcon={
+                  shareCopiedToClipboard ? <Check /> : <ShareVariant />
+                }
+                onClick={() => {
+                  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                  void shareUrl(log.downloadUrl!)
+                }}
+                sx={{ mr: 1 }}
+              >
+                {t(shareCopiedToClipboard ? 'log.clipboard' : 'log.share')}
+              </Button>
+              <Button
+                className="donate-button"
+                startIcon={<Heart />}
+                color="error"
+                href="https://afdian.net/@teahouse"
+                target="_blank"
+                rel="noopener"
+                sx={{ mr: 1 }}
+              >
+                {t('footer.donate')}
+              </Button>
+              <Tooltip title={t('log.howToInstall')}>
+                <IconButton
+                  href="https://lakeus.xyz/wiki/梗体中文/导入"
+                  target="_blank"
+                  rel="noopener"
+                  sx={{ float: 'right' }}
+                >
+                  <HelpCircle />
+                </IconButton>
+              </Tooltip>
+            </>
+          ) : (
+            <Button
+              variant="contained"
+              startIcon={<Bug />}
+              color="error"
+              href="https://github.com/Teahouse-Studios/mcwzh-meme-web-builder/issues/new/choose"
+              rel="noopener"
+              sx={{ mr: 1 }}
+            >
+              {t('log.feedback')}
+            </Button>
+          ))}
+      </AccordionDetails>
+    </Accordion>
+  )
+}
